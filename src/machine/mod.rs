59bc72412dbbc597af0d655e7785d3e5a82fc9d9
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-01-28 23:02:13 +0900

    Add test for StateMachine

--- src/machine/mod.rs
+++ src/machine/mod.rs
@@ -50,3 +50,124 @@ where
         self.current_state = new_state
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::marker::PhantomData;
+
+    use super::BasicStateMachine;
+    use super::StateMachine;
+
+    #[derive(Copy, Clone, Debug, PartialEq)]
+    enum Stations {
+        Shibuya,
+        IkejiriOhashi,
+        Sangendyaya,
+        KomazawaDaigaku,
+        Sakurashinmachi,
+        Yoga,
+        FutakoTamagawa,
+    }
+
+    enum Train {
+        Local,
+        Express,
+    }
+
+    #[test]
+    fn test_current_state() {
+        let sm = BasicStateMachine {
+            initial_state: Stations::Shibuya,
+            current_state: Stations::Shibuya,
+            transition: |station, train| match (station, train) {
+                (Stations::Shibuya, Train::Local) => Stations::IkejiriOhashi,
+                _ => unreachable!(),
+            },
+            _maker: PhantomData::<Train>::default(),
+        };
+
+        assert_eq!(Stations::Shibuya, sm.current_state());
+    }
+
+    #[test]
+    fn test_consume() {
+        let mut sm = BasicStateMachine {
+            initial_state: Stations::Shibuya,
+            current_state: Stations::Shibuya,
+            transition: |station, train| match (station, train) {
+                (Stations::Shibuya, Train::Local) => Stations::IkejiriOhashi,
+                (Stations::Shibuya, Train::Express) => Stations::Sangendyaya,
+                (Stations::IkejiriOhashi, Train::Local) => Stations::Sangendyaya,
+                (Stations::Sangendyaya, Train::Local) => Stations::KomazawaDaigaku,
+                (Stations::Sangendyaya, Train::Express) => Stations::FutakoTamagawa,
+                (Stations::KomazawaDaigaku, Train::Local) => Stations::Sakurashinmachi,
+                (Stations::Sakurashinmachi, Train::Local) => Stations::Yoga,
+                _ => unreachable!(),
+            },
+            _maker: PhantomData::<Train>::default(),
+        };
+
+        assert_eq!(Stations::IkejiriOhashi, sm.consume(Train::Local));
+    }
+
+    #[test]
+    fn test_peek() {
+        let mut sm = BasicStateMachine {
+            initial_state: Stations::Sangendyaya,
+            current_state: Stations::Sangendyaya,
+            transition: |station, train| match (station, train) {
+                (Stations::Shibuya, Train::Local) => Stations::IkejiriOhashi,
+                (Stations::Shibuya, Train::Express) => Stations::Sangendyaya,
+                (Stations::IkejiriOhashi, Train::Local) => Stations::Sangendyaya,
+                (Stations::Sangendyaya, Train::Local) => Stations::KomazawaDaigaku,
+                (Stations::Sangendyaya, Train::Express) => Stations::FutakoTamagawa,
+                (Stations::KomazawaDaigaku, Train::Local) => Stations::Sakurashinmachi,
+                (Stations::Sakurashinmachi, Train::Local) => Stations::Yoga,
+                _ => unreachable!(),
+            },
+            _maker: PhantomData::<Train>::default(),
+        };
+
+        assert_eq!(Stations::FutakoTamagawa, sm.peek(Train::Express));
+        assert_eq!(Stations::Sangendyaya, sm.current_state());
+    }
+
+    #[test]
+    fn test_reset() {
+        let mut sm = BasicStateMachine {
+            initial_state: Stations::Shibuya,
+            current_state: Stations::Sangendyaya,
+            transition: |station, train| match (station, train) {
+                (Stations::Shibuya, Train::Local) => Stations::IkejiriOhashi,
+                (Stations::Shibuya, Train::Express) => Stations::Sangendyaya,
+                (Stations::IkejiriOhashi, Train::Local) => Stations::Sangendyaya,
+                (Stations::Sangendyaya, Train::Local) => Stations::KomazawaDaigaku,
+                (Stations::Sangendyaya, Train::Express) => Stations::FutakoTamagawa,
+                (Stations::KomazawaDaigaku, Train::Local) => Stations::Sakurashinmachi,
+                (Stations::Sakurashinmachi, Train::Local) => Stations::Yoga,
+                _ => unreachable!(),
+            },
+            _maker: PhantomData::<Train>::default(),
+        };
+
+        assert_eq!(Stations::FutakoTamagawa, sm.consume(Train::Express));
+        assert_eq!(Stations::Shibuya, sm.reset());
+    }
+
+    #[test]
+    fn test_set() {
+        let mut sm = BasicStateMachine {
+            initial_state: Stations::Shibuya,
+            current_state: Stations::Shibuya,
+            transition: |station, train| match (station, train) {
+                (Stations::Shibuya, Train::Local) => Stations::IkejiriOhashi,
+                _ => unreachable!(),
+            },
+            _maker: PhantomData::<Train>::default(),
+        };
+
+        assert_eq!(Stations::Shibuya, sm.current_state());
+        sm.set(Stations::Yoga);
+        assert_eq!(Stations::Yoga, sm.current_state())
+    }
+}
