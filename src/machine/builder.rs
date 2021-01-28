35a04f73433ccd9710cf3a77633ce3bbf295b7c6
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-01-28 23:06:22 +0900

    Add StateMachineBuilder test

--- src/machine/builder.rs
+++ src/machine/builder.rs
@@ -54,3 +54,47 @@ where
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::machine::StateMachine;
+
+    use super::StateMachineBuilder;
+
+    #[allow(dead_code)]
+    #[derive(Copy, Clone, Debug, PartialEq)]
+    enum Stations {
+        Shibuya,
+        IkejiriOhashi,
+        Sangendyaya,
+        KomazawaDaigaku,
+        Sakurashinmachi,
+        Yoga,
+        FutakoTamagawa,
+    }
+
+    #[allow(dead_code)]
+    enum Train {
+        Local,
+        Express,
+    }
+
+    #[test]
+    fn test_build() {
+        let mut sm = StateMachineBuilder::start()
+            .initial_state(Stations::Shibuya)
+            .transition(|station, train| match (station, train) {
+                (Stations::Shibuya, Train::Local) => Stations::IkejiriOhashi,
+                (Stations::Shibuya, Train::Express) => Stations::Sangendyaya,
+                (Stations::IkejiriOhashi, Train::Local) => Stations::Sangendyaya,
+                (Stations::Sangendyaya, Train::Local) => Stations::KomazawaDaigaku,
+                (Stations::Sangendyaya, Train::Express) => Stations::FutakoTamagawa,
+                (Stations::KomazawaDaigaku, Train::Local) => Stations::Sakurashinmachi,
+                (Stations::Sakurashinmachi, Train::Local) => Stations::Yoga,
+                _ => unreachable!(),
+            })
+            .build();
+
+        assert_eq!(Stations::Sangendyaya, sm.consume(Train::Express));
+    }
+}
